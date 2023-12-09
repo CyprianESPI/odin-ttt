a3812fe56a2c5e3787d9488834a5c4192ee32298
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-09 16:56:57 +0100

    Switch CurrentPlayer

--- index.js
+++ index.js
@@ -25,15 +25,26 @@ console.log(BOARD);
 // Create players
 const p1 = new Player("p1", "X");
 const p2 = new Player("p2", "O");
-console.log(p1);
-console.log(p2);
+const PLAYERS = [p1, p2];
+let CurrentPlayer = PLAYERS[0];
+console.log(PLAYERS);
+
+function GetNextObj(array, obj) {
+    const currentIndex = array.indexOf(obj);
+    const nextIndex = (currentIndex + 1) % array.length;
+    return array[nextIndex];
+}
 
 /* Game - DOM interactions */
 const board = document.getElementById("board");
 for (let i = 0; i < SIZE; i++) {
     let row = []
     for (let j = 0; j < SIZE; j++) {
-        const e = document.createElement("div");
-        board.appendChild(e);
+        const elem = document.createElement("div");
+        elem.addEventListener("click", () => {
+            elem.innerText = CurrentPlayer.val;
+            CurrentPlayer = GetNextObj(PLAYERS, CurrentPlayer);
+        }, { once: true });
+        board.appendChild(elem);
     }
 }
